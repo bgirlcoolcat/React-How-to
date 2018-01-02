c585be5690db88c2b2d90e007160cb8cdc843e3d
bgirlcoolcat <evansb21@gmail.com>
2018-01-02 15:48:52 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+// A class component
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello World</h1>
+  }
+}
+
+// A stateless function component
+
+// const App = () => <h1>Hello stateless</h1>
+
+export default App;
