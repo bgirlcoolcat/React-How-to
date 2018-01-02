72db626f7398a17ae36133cfcb76850f2750387e
bgirlcoolcat <evansb21@gmail.com>
2018-01-02 16:31:58 +0000

    example of a stateless function component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
-// A class component
+// A stateless function component
 
-class App extends React.Component {
-  render() {
-    return <h1>Hello World</h1>
-  }
-}
+const App = () => <h1>Hello stateless</h1>
 
 export default App;
