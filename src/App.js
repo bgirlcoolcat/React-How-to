9148d214b0240c37d6a0a986905436e445c017a4
bgirlcoolcat <evansb21@gmail.com>
2018-01-03 12:53:40 +0000

    A React component can output or render other React components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import React from 'react';
 
-// Setting up state
+// An important feature of React is that a React component can output or render other React components
+// See notes in Evernote or video for explanation to be clear
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      txt: 'this is the state txt',
-      cat: 0
+      txt: 'this is the state txt'
     }
   }
-
-  // State is meant to be managed and updated by our component. 
-  // I'm going to create a method here called "update" that takes in an event.
-
   update(e) {
     this.setState({txt: e.target.value})
   }
-
   render() {
     return (
       <div>
-        <input type="text" 
-        onChange={this.update.bind(this)} />
-        <h1>{this.state.txt} - {this.state.cat}</h1>
+        <h1>{this.state.txt}</h1>
+        <Widget update={this.update.bind(this)}/>
+        {/* This also means that we could have a few of these guys on the page and every time we type in one of them, it'll update the value in our parent component. */}
+        <Widget update={this.update.bind(this)}/>
+        <Widget update={this.update.bind(this)}/>
       </div>
     )
   }
 }
+
+const Widget = (props) => 
+  <input type="text" onChange={props.update} />
 
 export default App;
