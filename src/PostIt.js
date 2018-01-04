ddf071c2998497b44d01b872fd80724bdad9c93f
bgirlcoolcat <evansb21@gmail.com>
2018-01-04 12:22:12 +0000

    Step 1 - Create and test class component works

---
+++ src/PostIt.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// 1. created my class PostIt for the post it notes component.
+
+class PostIt extends React.Component {
+
+    render() {
+        return (
+            <h1>Test my class works!</h1>
+        );
+    }
+}
+
+export default PostIt;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
-// import Welcome from './Welcome';
-// import Comment from './Comment';
-import App2 from './Welcome';
+
+// 1. import PostIt in giving file directory
+import PostIt from './PostIt';
 
 
-// const element = <Welcome name="Ally" name2="Bev" />;
 
-// const comment = {
-//     date: new Date(),
-//     text:
-//       'I hope you enjoy learning React!',
-//     author: {
-//       name: 'Hello Kitty',
-//       avatarUrl:
-//         'http://placekitten.com/g/64/64',
-//     },
-// };
-
+// Add the component (as a tag) to be rendered by React. Don't forget the comma.
 ReactDOM.render(
-    // <App />,
-    // element,
-    <App2 />,
-    // <Welcome />,
-    // <Comment
-    // date={comment.date}
-    // text={comment.text}
-    // author={comment.author}
-    // />,
-    // <h1>Bev!</h1>,
+    <PostIt />,
     document.getElementById('root')
 );
