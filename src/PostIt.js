8e9da36b797c4be1aab0b993e0e5ad852377cc1d
bgirlcoolcat <evansb21@gmail.com>
2018-01-04 12:34:07 +0000

    To make up my post it app, I include the elements in the render function

--- src/PostIt.js
+++ src/PostIt.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 // 1. created my class PostIt for the post it notes component.
+// 2. To make up my post it app, I include the following elements: 
+  // a div block with 2 buttons and some text. I include these elements in the render function.
 
 class PostIt extends React.Component {
 
     render() {
         return (
-            <h1>Test my class works!</h1>
+            <div className="commentContainer">
+                <div className="commentText">Your post it notes</div>
+                <button classname="button-primary">Edit</button>
+                <button className="button-danger">Remove</button>
+            </div>
         );
     }
 }
