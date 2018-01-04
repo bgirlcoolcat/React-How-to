75e6a0967e68f67c5d2d119e899d620c75507dc7
bgirlcoolcat <evansb21@gmail.com>
2018-01-04 17:12:26 +0000

    Set the initial state for editing

--- src/PostItState.js
+++ src/PostItState.js
@@ -3,12 +3,25 @@ import React from 'react';
 
 class PostItState extends React.Component {
 
+// 1. Set the initial state to be false for editing, so no forms shown by default, but just show text
+
+    state = { editing: false }
+
+// 2. When Edit button is clicked, we change the state and set editing to true
+
     edit = () => {
-        alert('Editing comment');
+        this.setState({ editing: true });
     }
 
     remove = () => {
-        alert('Removing comment');
+        console.log('Removing comment');
+    }
+
+// 3. We need another function for save so when in editing mode, we offer a save button (eventually to save the new text).
+  // For now, it will just take you out of editing mode by changing state so editing is set to false
+
+    save = () => {
+        this.setState({ editing: false });
     }
 
     render() {
