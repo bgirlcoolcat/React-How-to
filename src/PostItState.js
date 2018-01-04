ee1ac935c6368f1968f9464aca8ec6c527049bdc
bgirlcoolcat <evansb21@gmail.com>
2018-01-04 18:32:09 +0000

    Get the value of whatever the user typed in to the edit form using refs

--- src/PostItState.js
+++ src/PostItState.js
@@ -20,16 +20,22 @@ class PostItState extends React.Component {
 // 3. We need another function for save so when in editing mode, we offer a save button (eventually to save the new text).
   // For now, it will just take you out of editing mode by changing state so editing is set to false
 
+// 6b. Pass the ref value to the save function (saved in a variable). For now, we will use it in a console.log.
+    
     save = () => {
+        let value = this.refs.newText.value;
+        console.log('New postIt text:' + value);
         this.setState({ editing: false });
     }
 
 // 4. Create two render functions to toggle between the default mode and editing mode
 
+// 6a. Get the value of whatever the user typed in to the edit form using refs
+
     renderForm = () => {
         return (
             <div className="commentContainer">
-                <textarea defaultValue={this.props.children}></textarea>
+                <textarea ref="newText" defaultValue={this.props.children}></textarea>
                 <button onClick={this.save} classname="button-success">Save</button>
             </div>
         );
