a06bdb8532181c44e77a43318eb5a1b41e51bca0
bgirlcoolcat <evansb21@gmail.com>
2018-01-04 13:03:21 +0000

    In index.js, after step 4 in PostIt.js is done, we add a <div> tag and put multiple PostIt tags inside with content we want to display in app

--- src/index.js
+++ src/index.js
@@ -7,7 +7,14 @@ import PostIt from './PostIt';
 
 
 // Add the component (as a tag) to be rendered by React. Don't forget the comma.
+
+// After step 4 in PostIt.js is done, 
+// we add a <div></div> and put multiple PostIt tags inside with content we want to display in app
 ReactDOM.render(
-    <PostIt />,
+    <div>
+        <PostIt>Note 1</PostIt>
+        <PostIt>Note 2</PostIt>
+        <PostIt>Note 3</PostIt>
+    </div>,
     document.getElementById('root')
 );
